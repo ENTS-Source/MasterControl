462e963f63fb8f65942e72bfddc21fe58604d8b5
Travis Ralston <travpc@gmail.com>
2016-01-24 17:03:25 -0700

    Start of configuration code

--- mastercontrol.py
+++ mastercontrol.py
@@ -1,1 +1,23 @@
-print "TODO"
+import ConfigParser
+import time
+
+# Move globals to appropriate classes
+global session
+
+config = ConfigParser.ConfigParser()
+config.read('config/mastercontrol.ini')
+
+# Setup logging
+# TODO
+
+# Setup DB
+# TODO
+
+# Setup other components
+# TODO
+
+try:
+    while(True):
+        time.sleep(100)
+except KeyboardInterrupt:
+    print "^C received - shutting down server."
